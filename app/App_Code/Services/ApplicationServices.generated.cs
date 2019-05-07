8266f4c3773330fec5d97b07fdb51b685a6deedf
PlannedCompanies-DevTeam <DevOps@PlannedCompanies.com>
2019-05-07 16:09:46 -0400

    Add project files.

---
+++ app/App_Code/Services/ApplicationServices.generated.cs
@@ -0,0 +1,27 @@
+﻿using MyCompany.Handlers;
+using MyCompany.Web;
+using System.Web.Configuration;
+
+namespace MyCompany.Services
+{
+	public class AppFrameworkConfig
+    {
+        
+        public virtual void Initialize()
+        {
+            ApplicationServices.FrameworkAppName = "WF Finance App v2";
+            ApplicationServices.FrameworkSiteContentControllerName = "SiteContent";
+            ApplicationServices.Version = "8.7.10.7";
+            ApplicationServices.JqmVersion = "1.4.6";
+            ApplicationServices.HostVersion = "1.2.5.0";
+            var compilation = ((CompilationSection)(WebConfigurationManager.GetSection("system.web/compilation")));
+            var releaseMode = !compilation.Debug;
+            AquariumExtenderBase.EnableMinifiedScript = releaseMode;
+            AquariumExtenderBase.EnableCombinedScript = releaseMode;
+            ApplicationServices.EnableMinifiedCss = releaseMode;
+            ApplicationServices.EnableCombinedCss = releaseMode;
+            ApplicationServicesBase.AuthorizationIsSupported = false;
+            BlobFactoryConfig.Initialize();
+        }
+    }
+}
